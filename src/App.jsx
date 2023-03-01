91bc18d1a7939f6327d73af1de29230696d4b360
FDMtrainee <126129110+FDMtrainee@users.noreply.github.com>
2023-03-01 08:00:19 +0000

    with notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,24 @@
+import React , { Component } from 'react';
+
+// ** for demostration, irl you want components to be written in external files
+
+//classes
+class Welcome extends React.Component {
+  //constructor
+  constructor(props) {
+    super(props);
+  }
+  //render
+  render(){
+    return <h1>Meet the {this.props.name}</h1>
+  }
+}
+
+//Hooks
+const Hello = (props) => {
+  return <h1>Meet the <i style={{ color: "SteelBlue" }}>{props.name}</i></h1>
+}
+
 function App() {
   const name = 'StarGazers'
   return (
@@ -5,7 +26,12 @@ function App() {
       <article>
         <hgroup>
           <img src="images/group.svg" alt="StarGazers Group" />
-          <h1>Meet the <i style={{ color: "SteelBlue" }}>{name}</i></h1>
+          {/* <h1>Meet the <i style={{ color: "SteelBlue" }}>{name}</i></h1> */}
+          {/* begin with Capital case : Component (reusable) 
+              begin with lower case : html class */}
+          <Welcome name="Stars"/>
+          <Hello name="StarGazers"/>
+          <Welcome name="StarAliens"/>
           <p>Members of an <b>intergalactic alliance</b><br />
             paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
           <button className="outline" onClick={() => alert('Hi there')}>Click Me</button>
