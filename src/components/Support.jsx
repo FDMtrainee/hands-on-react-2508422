3649ae22211d54c1461d7e310a69a94673b18333
FDMtrainee <126129110+FDMtrainee@users.noreply.github.com>
2023-03-01 17:25:34 +0800

    notes with old way of writing condition

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -1,9 +1,17 @@
 import { useState } from 'react';
 
 export default () => {
+  // 0 here is to initialize 'count' variable with a value 0
   const [count, setCount] = useState(0)
   return (
     <button className="outline" onClick={() => setCount(count + 1)}>
+      //{( () => {
+      //  if (count === 0) {
+      //    return "Click to support";
+      //  } else {
+      //    return `Supported ${count} times`;
+      //  }
+      //})()}
       {count === 0 ? "Click to support": `Supported ${count} times`}
     </button>
   )
